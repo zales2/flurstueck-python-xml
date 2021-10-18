0c773cce17a6c91fa7184b9ac90599d6cb5d38f1
zales2 <77017523+zales2@users.noreply.github.com>
2021-10-18 10:30:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from flurstueck import Flurstueck
+from flurstueck import *
 
-filenameInput = input('Podaj nazwę pliku wraz z jego rozszerzniem (przyklad.xml): ')
-if '.json' in filenameInput:
-    filetypeInput = 'json'
-else:
-    filetypeInput = input('Podaj format danych (nas / aaa): ')
+file_list = os.listdir(PATH)
+while(True):
+    filenameInput = input('Podaj nazwę pliku wraz z jego rozszerzniem: ')
+    if filenameInput in file_list:
+        break
+    else:
+        print('Zła nazwa pliku, spróbuj ponownie!')
 
-flurstueck = Flurstueck(filenameInput, filetypeInput)
-flurstueck.search_in_file()
-flurstueck.get_dict()
+while(True):
+    filetypeInput = input( 'Podaj format danych (nas / aaa / json): ')
+    if filetypeInput in ['nas', 'aaa', 'json']:
+        break
+    else:
+        print('Nieprawidłowy format, spróbuj ponownie!')
 
+if filetypeInput == 'nas':
+    fluerstueck = NasType( filenameInput )
+elif filetypeInput == 'aaa':
+    fluerstueck = AaaType( filenameInput )
+elif filetypeInput == 'json':
+    fluerstueck = JsonType( filenameInput )
+
+fluerstueck.search_in_file()
+fluerstueck.get_dict()
