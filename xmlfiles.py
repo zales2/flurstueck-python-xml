4d7129ad84f99c7f5d7d3f64a6498382796c1e5d
zales2 <77017523+zales2@users.noreply.github.com>
2021-10-21 13:47:08 +0200

    Update xmlfiles.py

--- xmlfiles.py
+++ xmlfiles.py
@@ -2,9 +2,9 @@ from flurstueck import *
 import xml.etree.ElementTree as ET
 
 class XmlFiles(FileParent):
-    def __init__(self, filename):
-        super().__init__(filename)
-        self.file = ET.parse( PATH + filename ).getroot()
+    def __init__(self, filepath):
+        super().__init__(filepath)
+        self.file = ET.parse( filepath ).getroot()
         self.dict = {'Numer działki': '', 
                 'Wielkość działki': '', 
                 'Numer landu': '', 
@@ -12,11 +12,11 @@ class XmlFiles(FileParent):
                 'Numer powiatu': '',    
                 'Numer gemarkung': '' }
 
-    def get_data(self):
+    def execute(self):
         dictNames = iter(list(self.dict))        
         for elem in self.listType:
             for tag in list(self.file.iter(self.xmlns + elem)):
                 if self.xmlns + elem == tag.tag:
                     self.dict.update({next(dictNames): tag.text})
                     break
-
+        super().execute()
