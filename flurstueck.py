49b958662057f6682725fe60049ceda7f171f255
zales2 <77017523+zales2@users.noreply.github.com>
2021-10-21 13:46:01 +0200

    Update flurstueck.py

--- flurstueck.py
+++ flurstueck.py
@@ -1,19 +1,12 @@
 from abc import abstractmethod
-import os
 
-PATH = os.path.join('./files/')
-
-class TypeParent():
-    
+class TypeParent():  
     @abstractmethod
-    def get_data():
-        pass
-
-    def get_dict(self):
+    def execute(self):
         for attribute, value in self.dict.items():
             print(attribute, ': ', value)
 
 class FileParent(TypeParent):
-    def __init__(self, filename):
-        self.filename = filename
-
+    def __init__(self, filepath):
+        self.filepath = filepath
+    
