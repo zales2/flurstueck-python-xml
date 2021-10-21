defe6b4295261963a495eaad53eca7c25bf38393
zales2 <77017523+zales2@users.noreply.github.com>
2021-10-21 13:45:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from flurstueck import *
 from jsonType import JsonType
 from aaaType import AaaType
 from nasType import NasType
-
+import click
 
 types = { 'nas': NasType, 
     'aaa': AaaType, 
     'json': JsonType } # add new type
 
-while(True):
-    filetypeInput = input( 'Podaj format danych (nas / aaa / json): ')
-    if filetypeInput in types:
-        if issubclass(types[filetypeInput], FileParent):
-            file_list = os.listdir(PATH)
-            while(True):
-                filenameInput = input('Podaj nazwę pliku wraz z jego rozszerzniem: ')
-                if filenameInput in file_list:
-                    fluerstueck = types[filetypeInput](filenameInput)
-                    break
-                else:
-                    
-                    print('Zła nazwa pliku, spróbuj ponownie!')
-            break
-        
-        else:
-            # other type than file
-            # fluerstueck = types[filetypeInput](some new parameter/parameters)
-            break
-    else:
-        print('Nieprawidłowy format, spróbuj ponownie!')
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('type', type=click.Choice(list(types)))
+@click.argument('path')
+def process(type, path):
+    """ Get Flurstueck data\n
+    $ python3 main.py TYPE PATH"""
+    
+    try:
+        fluerstueck = types[type](path)
+        fluerstueck.execute()
+    except FileNotFoundError:
+        click.echo('No such file in directory')
 
-fluerstueck.get_data()
-fluerstueck.get_dict()
+if __name__ == '__main__':
+    process()
